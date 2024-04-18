77eff66a4ef46817b6c252b4a0c793e9b7b221ab
Blake Davies-Long <blake.davies.long@gmail.com>
2024-04-18 14:07:48 -0400

    updating profile page

---
+++ main.js
@@ -0,0 +1,11 @@
+const emailButton = document.getElementById('email');
+const gitHubButton = document.getElementById('github');
+const phoneButton = document.getElementById('phone');
+
+/*const moveDown = (event) => {
+    event.target.style.
+}
+
+emailButton.addEventListener('mouseover', );
+gitHubButton.addEventListener('mouseover');
+phoneButton.addEventListener('mouseover');*/
